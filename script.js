17368c2a06ee6c92c0952f1d6ab5aad3d9183d37
Roxie Charchian <rmcharchian@gmail.com>
2018-02-26 09:59:07 -0600

    end variables and data types

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
