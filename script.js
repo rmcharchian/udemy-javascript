74dc6974046dbc8de05c57532f18fbd4f32cea20
Roxie Charchian <rmcharchian@gmail.com>
2018-03-03 07:24:06 -0600

    end Loops and Iteration

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ console.log(john);
 */
 
 //v2.0
+/*
 var john = {
     name: 'John',
     lastName: 'Smith',
@@ -301,5 +302,68 @@ var john = {
 
 john.calculateAge();
 console.log(john);
- 
+*/
 
+//Lecture: Loops and Iteration
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+ /* 
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 9
+10, false, end loop!
+ */
+/*
+ var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+ for (var i=0; i < 5; i++) {
+     console.log(names[i]);
+ }
+ // When we know how many names are in the array
+
+ var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+ for (var i=0; i < names.length; i++) {
+     console.log(names[i]);
+ }
+
+ //When we don't know how many items are in the array, we use .length to catch whole array
+
+ for (var i=names.length -1; i >= 0; i--) {
+    console.log(names[i]);
+}
+*/
+// when we want to start with a specific item [4] and we want to begin at the end
+
+//While loops
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+var i = 0; //if we want a counter
+
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+for (var i=0; i<=5; i++) {
+    console.log(i);
+
+    if (i === 3) {
+        break;
+    }
+}
+
+
+for (var i=0; i<=5; i++) {
+
+    if (i === 3) {
+        continue;
+    }
+
+    console.log(i);
+}
