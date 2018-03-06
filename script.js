8e3832994283b3a2aa4cc61b0ed86e6a4a841022
Roxie Charchian <rmcharchian@gmail.com>
2018-03-05 19:16:00 -0600

    The this keyword in practice end

--- script.js
+++ script.js
@@ -451,4 +451,39 @@ function third() {
 }
 */
 
-//Lecture: 
+//Lecture: The 'this' keyword
+
+//console.log(this);
+/*
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2016- year);
+    console.log(this);
+}
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+}
+
+//method borrow
+mike.calculateAge = john.calculateAge;  //we want to borrow the function from john to use with Mike
+mike.calculateAge();  //we need to call it
+*/
